6eac86944e0c1eaedd17033a8b6411d771856158
Nathaniel DiRenzo <88605064+NateDiR@users.noreply.github.com>
2022-09-30 18:11:38 -0400

    big commit

---
+++ dags/acled_dag.py
@@ -0,0 +1,36 @@
+""""ACLED Data Ingest DAG"""
+import os
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
+from scripts.ingest_data import ingest_data
+
+email = os.getenv("EMAIL_ADDRESS")
+
+with DAG(
+    dag_id="acled_data_ingest",
+    start_date=datetime(2022, 9, 28),
+    schedule_interval="0 0 * * 3",
+    catchup=True,
+    default_args={
+        "retries": 1,
+        "retry_delay": timedelta(minutes=5),
+        "email_on_failure": False,
+        "email_on_retry": False,
+        "email": email,
+    },
+) as dag:
+
+    start_task = EmptyOperator(task_id="acled_start_task", dag=dag)
+
+    ingest_task = PythonOperator(
+        task_id="acled_ingest_task",
+        python_callable=ingest_data,
+        op_args={" {{ data_interval_start.int_timestamp }} "},
+        dag=dag,
+    )
+
+    end_task = EmptyOperator(task_id="acled_end_task", dag=dag)
+
+    start_task >> ingest_task >> end_task
---
+++ dags/scripts/ingest_data.py
@@ -0,0 +1,54 @@
+""""ACLED Data Ingest Function"""
+import os
+from datetime import datetime
+from pyspark.sql import SparkSession
+from pyspark import SparkConf
+from pyspark.sql import functions as F
+from pyspark.sql.types import StructType
+import requests
+from dotenv import load_dotenv
+
+def ingest_data(date):
+    """Ingest ACLED data for a given date"""
+    load_dotenv()
+
+    aws_access_key = os.getenv("AWS_ACCESS_KEY")
+    aws_secret_key = os.getenv("AWS_SECRET_KEY")
+    api_key = os.getenv("ACLED_ACCESS_KEY")
+    username = os.getenv("ACLED_USERNAME")
+
+    conf = SparkConf()
+    conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.3.4')
+    conf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider')
+    conf.set('spark.hadoop.fs.s3a.access.key', aws_access_key)
+    conf.set('spark.hadoop.fs.s3a.secret.key', aws_secret_key)
+
+    spark = SparkSession.builder.config(conf=conf).getOrCreate()
+    dataframe = spark.createDataFrame([], StructType([]))
+
+    response = requests.get(f"""https://api.acleddata.com/acled/read?key={api_key}&email={username}&timestamp={date}/{datetime.fromtimestamp(int(date)).strftime("%Y-%m-%d")}&iso=804""", timeout=30)
+    data = response.json()['data']
+    dataframe2 = spark.read.json(spark.sparkContext.parallelize([data]))
+    dataframe = dataframe.unionByName(dataframe2, True)
+    if dataframe.count() == 0:
+        return "No data"
+
+    dataframe = dataframe.withColumn("event_date", F.to_date("event_date", "yyyy-MM-dd"))
+    dataframe = dataframe.withColumn("fatalities", dataframe["fatalities"].cast("int"))
+    dataframe = dataframe.withColumn("geo_precision", dataframe["geo_precision"].cast("int"))
+    dataframe = dataframe.withColumn("inter1", dataframe["inter1"].cast("int"))
+    dataframe = dataframe.withColumn("inter2", dataframe["inter2"].cast("int"))
+    dataframe = dataframe.withColumn("interaction", dataframe["interaction"].cast("int"))
+    dataframe = dataframe.withColumn("latitude", dataframe["latitude"].cast("double"))
+    dataframe = dataframe.withColumn("longitude", dataframe["longitude"].cast("double"))
+    dataframe = dataframe.withColumn("time_precision", dataframe["time_precision"].cast("int"))
+    dataframe = dataframe.withColumnRenamed("timestamp", "upload_date")
+    dataframe = dataframe.withColumn("upload_date", F.from_unixtime("upload_date", "yyyy-MM-dd HH:mm:ss"))
+    dataframe = dataframe.withColumn("upload_date", F.to_timestamp("upload_date", "yyyy-MM-dd HH:mm:ss"))
+    dataframe = dataframe.withColumn("year", F.year("year"))
+
+    for column in dataframe.columns:
+        dataframe = dataframe.withColumn(column, F.when(F.col(column) == "", None).otherwise(F.col(column)))
+
+    dataframe.coalesce(1).write.partitionBy('event_date').mode("append").option("mapreduce.fileoutputcommitter.marksuccessfuljobs", "false").parquet('s3a://acled-bucket/parquet/')
+    return "Success"
