bf167398c8c9914ebedb66caac1122f6b7160b47
Nathaniel DiRenzo <88605064+NateDiR@users.noreply.github.com>
2022-10-04 21:12:56 -0400

    update streamlit cols

--- streamlit_script/streamlit_app.py
+++ streamlit_script/streamlit_app.py
@@ -15,8 +15,8 @@ aws_region = os.getenv("AWS_REGION")
 aws_access_key = os.getenv("AWS_ACCESS_KEY")
 aws_secret_key = os.getenv("AWS_SECRET_KEY")
 
-col1 = st.columns(1)
-    
+col = st.columns(1)
+
 @st.cache
 def get_daily_data(date):
     """Query Athena and return results as a Pandas dataframe"""
@@ -28,7 +28,7 @@ def get_daily_data(date):
         cursor_class=PandasCursor,
     ).cursor()
     return athena.execute(f"""SELECT * FROM {acled_db}.{acled_table} WHERE event_date LIKE '{date}'""").as_pandas()
-with col1:
+with col[0]:
     date_choice = st.selectbox("Choose Date", [strftime("%Y-%m-%d", d.timetuple()) for d in date_range(start="2022-02-24", end=datetime.today())])
 
     if st.button("Generate Table"):
