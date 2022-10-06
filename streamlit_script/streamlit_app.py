6f8bcc6c18e3040dedbfeb6ab04f161b9f494ff4
Nathaniel DiRenzo <88605064+NateDiR@users.noreply.github.com>
2022-10-05 21:45:26 -0400

    streamlit update

--- streamlit_script/streamlit_app.py
+++ streamlit_script/streamlit_app.py
@@ -35,7 +35,7 @@ def get_daily_data(date):
         f"""SELECT * FROM {acled_db}.{acled_table} WHERE event_date LIKE '{date}'"""
     ).as_pandas()
 
-with st.sidebar.title("Filter by date"):
+with st.sidebar.title(title="Filter Data"):
         date_choice = st.selectbox(
         "Choose Date",
         [
@@ -48,7 +48,7 @@ with st.sidebar.title("Filter by date"):
             )
         ],
     )
-        with st.sidebar:
+        with st.sidebar(title="Filter"):
             gen_dash = st.button("Generate Dashboard")
 
 with col[0]:
