85e5fdb9633ffef46e5d5d1f5308f20be9f36ade
Nathaniel DiRenzo <88605064+NateDiR@users.noreply.github.com>
2022-10-08 17:58:10 -0400

    streamlit update

--- streamlit_script/streamlit_app.py
+++ streamlit_script/streamlit_app.py
@@ -45,7 +45,7 @@ with st.sidebar:
         "Choose Start Date",
         (
             datetime(2022, 2, 24),
-            datetime(datetime.today() - timedelta(days=8)),
+            (datetime.today() - timedelta(days=8)),
         ),
         datetime(2022, 2, 24),
         (datetime.today() - timedelta(days=8)),
